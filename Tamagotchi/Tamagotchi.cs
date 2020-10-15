61df03e5eb5261d16ecda70a6a1a60b440c49ccd
Clara01m <clara.mahlgard.sthlm.ntig@gmail.com>
2020-10-15 16:10:27 +0200

    added to the functions and fixed some minor confusion

--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -6,28 +6,59 @@ namespace Tamagochi
     {
         int Hunger = 0;
         int Boredome = 0;
-        static List<string> words = new List<string>();
+        public List<string> words = new List<string>();
         bool isAlive = true;
         static Random generator = new Random();
         public string Name = "----";
 
-        public bool GetAlive = true;
+        public bool GetAlive ()
+        {
+            if (isAlive = true)
+            {
+                Console.WriteLine(first.Name + " is alive ^3^");
+            }
+            else
+            {
+                Console.WriteLine("Oh no! it's funeral time..."
+                + " Or you could restart the game by exiting and entering again"); 
+            }
+        }
 
         public void Feed()
-        { }
+        {
+            Hunger--;
+        }
+
         public void Hi()
-        { }
+        { 
+            
+            ReduceBoredom;
+        }
+
         public void Teach(string word)
         {
             words.Add(word); 
-
+            ReduceBoredom;
         }
+
         public void Tick()
-        { }
+        {
+            Hunger ++; 
+            Boredome ++;
+            if (Hunger>10 || Boredome>10)
+            {
+                isAlive = false; 
+            }
+        }
+
         public void PrintStats()
-        { }
+        { 
+            Console.WriteLine(GetAlive + first.Hunger + first.Boredome);
+        }
         static void ReduceBoredom()
-        { }
+        { 
+            Boredome --; 
+        }
 
     }
 }
